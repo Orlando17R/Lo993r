7d02655b2b9ab467b2e04498f5748b44cc931444
OrlandoRodriguez <orlando.ramiro11@gmail.com>
2017-11-14 23:59:08 -0400

    11 59 14 nov

--- Keylogger/main.cpp
+++ Keylogger/main.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
+#include<iostream>
+#include<windows.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello orlando!" << endl;
+    MSG Msg;
+
+    while(GetMessage(&Msg, NULL, 0,0) )
+    {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+
     return 0;
 }
